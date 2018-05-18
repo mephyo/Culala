aa4357ce67082eaefade6cb0f662d304a6f5d307
尤雨尘 <hergloves@gmail.com>
2018-05-18 19:06:35 +0800

    new

--- src/components/Appliers.vue
+++ src/components/Appliers.vue
@@ -66,21 +66,21 @@
                         </div>
                     </th>
                     <th>
-                        <div class="cell-inline">
+                        <div class="cell-inline" title="操作">
                             <svg class="naked-icon-white" viewBox="0 0 24 24">
                                 <path d="M1.39,18.36L3.16,16.6L4.58,18L5.64,16.95L4.22,15.54L5.64,14.12L8.11,16.6L9.17,15.54L6.7,13.06L8.11,11.65L9.53,13.06L10.59,12L9.17,10.59L10.59,9.17L13.06,11.65L14.12,10.59L11.65,8.11L13.06,6.7L14.47,8.11L15.54,7.05L14.12,5.64L15.54,4.22L18,6.7L19.07,5.64L16.6,3.16L18.36,1.39L22.61,5.64L5.64,22.61L1.39,18.36Z" />
                             </svg>
                         </div>
                     </th>
                     <th>
-                        <div class="cell-inline">
+                        <div class="cell-inline" title="备注">
                             <svg class="naked-icon-white" viewBox="0 0 24 24">
                                 <path d="M12,16A4,4 0 0,1 8,12A4,4 0 0,1 12,8A4,4 0 0,1 16,12A4,4 0 0,1 12,16M18.7,12.4C18.42,12.24 18.13,12.11 17.84,12C18.13,11.89 18.42,11.76 18.7,11.6C20.62,10.5 21.69,8.5 21.7,6.41C19.91,5.38 17.63,5.3 15.7,6.41C15.42,6.57 15.16,6.76 14.92,6.95C14.97,6.64 15,6.32 15,6C15,3.78 13.79,1.85 12,0.81C10.21,1.85 9,3.78 9,6C9,6.32 9.03,6.64 9.08,6.95C8.84,6.75 8.58,6.56 8.3,6.4C6.38,5.29 4.1,5.37 2.3,6.4C2.3,8.47 3.37,10.5 5.3,11.59C5.58,11.75 5.87,11.88 6.16,12C5.87,12.1 5.58,12.23 5.3,12.39C3.38,13.5 2.31,15.5 2.3,17.58C4.09,18.61 6.37,18.69 8.3,17.58C8.58,17.42 8.84,17.23 9.08,17.04C9.03,17.36 9,17.68 9,18C9,20.22 10.21,22.15 12,23.19C13.79,22.15 15,20.22 15,18C15,17.68 14.97,17.36 14.92,17.05C15.16,17.25 15.42,17.43 15.7,17.59C17.62,18.7 19.9,18.62 21.7,17.59C21.69,15.5 20.62,13.5 18.7,12.4Z" />
                             </svg>
                         </div>
                     </th>
                     <th>
-                        <div class="cell-inline">
+                        <div class="cell-inline" title="操作">
                             <svg class="naked-icon-white" viewBox="0 0 24 24">
                                 <path d="M13,14H11V10H13M13,18H11V16H13M1,21H23L12,2L1,21Z" />
                             </svg>
@@ -223,20 +223,22 @@
                         </div>
                     </td>
                     <td>
-                        <span v-if="a.phyLimit===4">全裸并露点</span>
-                        <span v-if="a.phyLimit===3">全裸然而不露点</span>
-                        <span v-if="a.phyLimit===2">半裸</span>
-                        <span v-if="a.phyLimit===1">内衣</span>
+                        <span v-if="a.phyLimit==4">全裸并露点</span>
+                        <span v-if="a.phyLimit==3">全裸然而不露点</span>
+                        <span v-if="a.phyLimit==2">半裸</span>
+                        <span v-if="a.phyLimit==1">内衣</span>
                     </td>
+                    <!-- 尺度 -->
                     <td>
-                        <span v-if="a.limit===4">色情又变态</span>
-                        <span v-if="a.limit===3">有欲望但不色情</span>
-                        <span v-if="a.limit===2">没有欲望的美好肉体</span>
-                        <span v-if="a.limit===1">性冷淡</span>
+                        <span v-if="a.limit==4">色情又变态</span>
+                        <span v-if="a.limit==3">有欲望但不色情</span>
+                        <span v-if="a.limit==2">没有欲望的美好肉体</span>
+                        <span v-if="a.limit==1">性冷淡</span>
                     </td>
+                    <!-- 操作 -->
                     <td>
                         <div class="cell-inline">
-                            <div class="icon-button-black" @click="deleteMe(v._id)" title="Delete This Record">
+                            <div class="icon-button-black" @click="deleteMe(a._id)" title="Delete This Record">
                                 <svg viewBox="0 0 24 24">
                                     <path d="M19.36,2.72L20.78,4.14L15.06,9.85C16.13,11.39 16.28,13.24 15.38,14.44L9.06,8.12C10.26,7.22 12.11,7.37 13.65,8.44L19.36,2.72M5.93,17.57C3.92,15.56 2.69,13.16 2.35,10.92L7.23,8.83L14.67,16.27L12.58,21.15C10.34,20.81 7.94,19.58 5.93,17.57Z" />
                                 </svg>
@@ -266,7 +268,6 @@
                         item.copyright = Number(item.copyright)
                     });
                     this.appliers = data.reverse()
-                    console.log(this.appliers)
                 })
             },
             deleteMe(id) {
